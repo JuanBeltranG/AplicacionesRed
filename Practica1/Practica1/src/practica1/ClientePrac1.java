8e39c1b21304b62693e7ea34175c4cfe61b08d07
Juan Beltran Garcia <jbeltrang1603@alumno.ipn.mx>
2022-03-06 22:15:26 -0600

    menu subir carpteta

--- Practica1/Practica1/src/practica1/ClientePrac1.java
+++ Practica1/Practica1/src/practica1/ClientePrac1.java
@@ -11,51 +11,84 @@ import java.net.Socket;
 import javax.swing.JFileChooser;
 
 public class ClientePrac1 {
-    
-    
-    
-    public static void consultaRepoLocal(){
-        
+
+    public static void consultaRepoLocal() {
+
         //Con el sig segmento de codigo obtenemos la ruta a nuestra carpeta que contiene el repo local
-          File f = new File("");
-          String ruta = f.getAbsolutePath();
-          String carpeta="RepositorioCliente";
-          String rutaRepoLocal = ruta+"\\"+carpeta+"\\";
-          
+        File f = new File("");
+        String ruta = f.getAbsolutePath();
+        String carpeta = "RepositorioCliente";
+        String rutaRepoLocal = ruta + "\\" + carpeta + "\\";
+
         //Con el sig segmento de codigo obtendremos todos los archivos y directorios del repo local y los imprimiremos
         File folder = new File(rutaRepoLocal);
-        
+
         for (File fileEntry : folder.listFiles()) {
             if (fileEntry.isDirectory()) {
-                System.out.println("Directorio: " + fileEntry.getName() );
+                System.out.println("Directorio: " + fileEntry.getName());
             } else {
                 System.out.println("Archivo: " + fileEntry.getName());
             }
         }
     }
-    
-    
-    
-    
-    
-    
-    public static void subirArchivosCarpetas(){
-        
-        //DE MOMENTO ESTO SOLO ENVIA ARCHIVOS AUN NO ENVIA VARIOS ARCHIVOS NI DIRECTORIOS ENTEROS
-        try{
+
+    public static void subirCarpeta() {
+
+        try {
             int pto = 8000;
             String dir = "127.0.0.1";
-            Socket cl = new Socket(dir,pto);
+            Socket cl = new Socket(dir, pto);
+            System.out.println("Conexion con servidor establecida.. lanzando FileChooser..");
+
+            JFileChooser chooser;
+            String choosertitle = new String();
+
+            chooser = new JFileChooser();
+            chooser.setCurrentDirectory(new java.io.File("."));
+            chooser.setDialogTitle(choosertitle);
+            chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+            chooser.setAcceptAllFileFilterUsed(false);
+            int r = chooser.showOpenDialog(null);
+
+            //El chooser.getSelected file es el que hara referencia a la carpeta que subiremos
+            if (r == JFileChooser.APPROVE_OPTION) {
+                System.out.println("getCurrentDirectory(): "
+                        + chooser.getCurrentDirectory());
+                System.out.println("getSelectedFile() : "
+                        + chooser.getSelectedFile());
+            } else {
+                System.out.println("No Selection ");
+            }
+            
+            
+            
+            
+            
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void subirArchivosCarpetas() {
+
+        //DE MOMENTO ESTO SOLO ENVIA ARCHIVOS AUN NO ENVIA VARIOS ARCHIVOS NI DIRECTORIOS ENTEROS
+        try {
+            int pto = 8000;
+            String dir = "127.0.0.1";
+            Socket cl = new Socket(dir, pto);
             System.out.println("Conexion con servidor establecida.. lanzando FileChooser..");
             JFileChooser jf = new JFileChooser();
-            //jf.setMultiSelectionEnabled(true);
+            jf.setMultiSelectionEnabled(true);
             int r = jf.showOpenDialog(null);
-            if(r==JFileChooser.APPROVE_OPTION){
+            if (r == JFileChooser.APPROVE_OPTION) {
                 File f = jf.getSelectedFile();
                 String nombre = f.getName();
                 String path = f.getAbsolutePath();
                 long tam = f.length();
-                System.out.println("Preparandose pare enviar archivo "+path+" de "+tam+" bytes\n\n");
+                System.out.println("Preparandose pare enviar archivo " + path + " de " + tam + " bytes\n\n");
                 DataOutputStream dos = new DataOutputStream(cl.getOutputStream());
                 DataInputStream dis = new DataInputStream(new FileInputStream(path));
                 dos.writeUTF(nombre);
@@ -63,38 +96,38 @@ public class ClientePrac1 {
                 dos.writeLong(tam);
                 dos.flush();
                 long enviados = 0;
-                int l=0,porcentaje=0;
-                while(enviados<tam){
+                int l = 0, porcentaje = 0;
+                while (enviados < tam) {
                     byte[] b = new byte[1500];
-                    l=dis.read(b);
-                    System.out.println("enviados: "+l);
-                    dos.write(b,0,l);
+                    l = dis.read(b);
+                    System.out.println("enviados: " + l);
+                    dos.write(b, 0, l);
                     dos.flush();
                     enviados = enviados + l;
-                    porcentaje = (int)((enviados*100)/tam);
-                    System.out.print("\rEnviado el "+porcentaje+" % del archivo");
+                    porcentaje = (int) ((enviados * 100) / tam);
+                    System.out.print("\rEnviado el " + porcentaje + " % del archivo");
                 }//while
                 System.out.println("\nArchivo enviado..");
                 dis.close();
                 dos.close();
                 cl.close();
             }
-            
-        }catch(Exception e){
+
+        } catch (Exception e) {
             e.printStackTrace();
-            
+
         }
-        
+
     }
 
     public static void main(String[] args) throws IOException {
-        
+
         //Comenzaremos mostrando el menu de las opciones disponibles para el usuario
         int option = 0;
         while (option != 8) {
-            
+
             System.out.println("BIENVENIDO A TU REPOSITORIO DE ARCHIVOS");
-            
+
             System.out.println("Estas son las operaciones disponibles");
             System.out.println("-----------------------------------------------------------------------------------");
             System.out.println("1.- Listar contenido de mi repositorio local");
@@ -106,35 +139,38 @@ public class ClientePrac1 {
             System.out.println("7.- Modificar parametros de conexión con el repositorio remoto");
             System.out.println("8.- Salir");
             System.out.println("-----------------------------------------------------------------------------------");
-            
+
             System.out.println("Introduce el numero de la opcion a realizar: ");
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             String op = reader.readLine();
             option = Integer.parseInt(op);
-            
-            switch (option) 
-            {
-                case 1:  consultaRepoLocal();
-                         break;
-                case 2:  
-                         break;
-                case 3: subirArchivosCarpetas();
-                         break;
-                case 4:  
-                         break;
-                case 5:  
-                         break;
-                case 6:  
-                         break;
-                case 7:  
-                         break;
-                case 8:  System.out.println("Hata la proxima");
-                         break;
-                default: System.out.println("Operación no reconocida, por favor intentelo de nuevo");
-                         break;
+
+            switch (option) {
+                case 1:
+                    consultaRepoLocal();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    subirCarpeta();
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    System.out.println("Hata la proxima");
+                    break;
+                default:
+                    System.out.println("Operación no reconocida, por favor intentelo de nuevo");
+                    break;
             }
-            
+
         }
 
     }
